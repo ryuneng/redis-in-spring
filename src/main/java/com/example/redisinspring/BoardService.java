008966eefa2e6ef24c0e9bc0d5dbab2023c5c8d9
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-10-09 01:19:38 +0900

    섹션5 | Spring Boot 프로젝트에 Redis 세팅 추가하기

--- src/main/java/com/example/redisinspring/BoardService.java
+++ src/main/java/com/example/redisinspring/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.redisinspring;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,8 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    // key 예시) boards:page:1:size:10 / cacheManager : RedisCacheConfig에 CacheManager 타입으로 정의한 메소드명과 일치해야 함
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
 
         Pageable pageable = PageRequest.of(page - 1, size);
